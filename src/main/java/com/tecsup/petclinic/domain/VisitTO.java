5152d80d73d51aa21fc44bbe8c46ebb06f9106ae
Arturo2209 <arturobullonsupanta@gmail.com>
2024-11-07 23:45:15 -0500

    Descripción de los cambios realizados

--- src/main/java/com/tecsup/petclinic/domain/VisitTO.java
+++ src/main/java/com/tecsup/petclinic/domain/VisitTO.java
@@ -1,4 +1,25 @@
 package com.tecsup.petclinic.domain;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class VisitTO {
+
+    private Integer id;
+
+    private Long petId;
+
+    private String visitDate;
+
+    private String description;
+
+
 }
--- src/main/java/com/tecsup/petclinic/mapper/VisitMapper.java
+++ src/main/java/com/tecsup/petclinic/mapper/VisitMapper.java
@@ -1,4 +1,49 @@
 package com.tecsup.petclinic.mapper;
 
+import com.tecsup.petclinic.domain.VisitTO;
+import com.tecsup.petclinic.entities.Visit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface VisitMapper {
+
+    VisitMapper INSTANCE = Mappers.getMapper(VisitMapper.class);
+
+    @Mapping(source = "visitDate", target = "visitDate")
+    Visit toVisit(VisitTO visitTO);
+
+    default Date stringToDate(String dateStr) {
+        Date date = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            date = dateFormat.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    @Mapping(source = "visitDate", target = "visitDate")
+    VisitTO toVisitTO(Visit visit);
+
+    default String dateToString(Date date) {
+        if (date != null) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            return dateFormat.format(date);
+        } else {
+            return "";
+        }
+    }
+
+    List<VisitTO> toVisitTOList(List<Visit> visitList);
+
+    List<Visit> toVisitList(List<VisitTO> visitTOList);
 }
